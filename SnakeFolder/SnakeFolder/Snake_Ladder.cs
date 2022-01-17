62a0adee52b7aefd95641cb36338325df59ceeac
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-17 19:47:03 +0530

    [Vinayak] checking weather player have to move or not

--- SnakeFolder/SnakeFolder/Snake_Ladder.cs
+++ SnakeFolder/SnakeFolder/Snake_Ladder.cs
@@ -11,12 +11,31 @@ namespace SnakeFolder
         int FirstPlayerPosition;
 
         Random random = new Random();
+        int ranOption;
         public void game()
         {
             //First player starting from position 0
             FirstPlayerPosition = 0;
             //Taking random value of Dice from 1-6
             int DiceValue = random.Next(1, 7);
+
+            ranOption = random.Next(0, 3);
+
+            if (ranOption == 0)
+            {
+                Console.WriteLine("no play");
+                FirstPlayerPosition += FirstPlayerPosition;
+            }
+            else if (ranOption == 1)
+            {
+                Console.WriteLine("Ladder");
+                FirstPlayerPosition += FirstPlayerPosition + DiceValue;
+            }
+            else
+            {
+                Console.WriteLine("Snake");
+                FirstPlayerPosition -= FirstPlayerPosition + DiceValue;
+            }
         }
     }
 }
