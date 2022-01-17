a55d1d854fa1ffd6a5af6081f13094d27b351025
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-17 19:31:42 +0530

    [Vinayak] adding starting position of snake ladder program

---
+++ SnakeFolder/SnakeFolder/Snake_Ladder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeFolder
+{
+    public class Snake_Ladder
+    {
+        int FirstPlayerPosition;
+        public void game()
+        {
+            //First player starting from position 0
+            FirstPlayerPosition = 0;
+            
+        }
+    }
+}
