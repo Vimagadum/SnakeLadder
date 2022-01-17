568d1d80f3ce9a5ffef47a8254378bebc7667a42
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-17 19:38:32 +0530

    [Vinayak] taking random value in snake ladder program

--- SnakeFolder/SnakeFolder/Snake_Ladder.cs
+++ SnakeFolder/SnakeFolder/Snake_Ladder.cs
@@ -9,11 +9,14 @@ namespace SnakeFolder
     public class Snake_Ladder
     {
         int FirstPlayerPosition;
+
+        Random random = new Random();
         public void game()
         {
             //First player starting from position 0
             FirstPlayerPosition = 0;
-            
+            //Taking random value of Dice from 1-6
+            int DiceValue = random.Next(1, 7);
         }
     }
 }
